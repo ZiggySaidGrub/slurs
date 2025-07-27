c0a4a5a006051d62041499451e88014b1192c31b
ZiggySaidGrub <grubsaidgrub@gmail.com>
2025-07-27 12:39:43 -0600

    oopsnewurl 2, electric boogaloo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = new XMLHttpRequest();
 
 try {
-    request.open("GET", "https://meow.smots.horse:6666/thesilly");
+    request.open("GET", "https://meow.smots.horse:6666");
 
 
     request.addEventListener("load", () => {
