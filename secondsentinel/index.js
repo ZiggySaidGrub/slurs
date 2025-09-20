2b1eac2523f3aac6598a1f48acdafce2c2b8d51e
ZiggySaidGrub <grubsaidgrub@gmail.com>
2025-09-20 14:26:04 -0600

    AAA THE LAG AAAAA

--- secondsentinel/index.js
+++ secondsentinel/index.js
@@ -1,5 +1,4 @@
 let sparkles = false;
-let siner = 0;
 
 document.body.addEventListener("click", (e) => {
     sparkles = true
@@ -13,18 +12,20 @@ setInterval(() => {
         sparkle.width = 100 + (Math.random() * 75)
         sparkle.style.position = "absolute";
         sparkle.style.left = "25px"
-        sparkle.style.bottom = "0px";
+        sparkle.style.bottom = `-200px`;
         document.body.appendChild(sparkle);
     }
 }, 1500);
 
 setInterval(() => {
-    siner += 0.01
     if (sparkles) {
         let sparkleList = document.querySelectorAll(".sparkle");
         sparkleList.forEach((sparkle) => {
             sparkle.style.bottom = `${parseFloat(sparkle.style.bottom)+1.5}px`
-            sparkle.style.left = `${(Math.sin(parseFloat(sparkle.style.bottom) * 0.01) * 20) + 25}px`
+            sparkle.style.left = `${(Math.sin(parseFloat(sparkle.style.bottom) * 0.01) * 20) + 50}px`
+            if (parseFloat(sparkle.style.bottom) > window.innerHeight) {
+                sparkle.remove()
+            }
         });
     }
 }, 10);
@@ -38,18 +39,20 @@ setInterval(() => {
         sparkle.width = 100 + (Math.random() * 75)
         sparkle.style.position = "absolute";
         sparkle.style.right = "25px"
-        sparkle.style.bottom = "0px";
+        sparkle.style.bottom = "-200px";
         document.body.appendChild(sparkle);
     }
 }, 1500);
 
 setInterval(() => {
-    siner += 0.01
     if (sparkles) {
         let sparkleList = document.querySelectorAll(".sparkleR");
         sparkleList.forEach((sparkle) => {
             sparkle.style.bottom = `${parseFloat(sparkle.style.bottom)+1.5}px`
-            sparkle.style.right = `${(Math.sin(parseFloat(sparkle.style.bottom) * 0.01) * 20) + 25}px`
+            sparkle.style.right = `${(Math.sin(parseFloat(sparkle.style.bottom) * 0.01) * 20) + 50}px`
+            if (parseFloat(sparkle.style.bottom) > window.innerHeight) {
+                sparkle.remove()
+            }
         });
     }
 }, 10);
